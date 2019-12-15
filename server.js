e5e6f561f6d018293132ff27d5bc78946a7c2920
ZITAVISH <rounakdas2014@gmail.com>
2019-12-15 15:57:46 +0530

    Node Commit Sync Fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const db2con=require('./db2test');
 const express=require("express");
-const Promise=require("bluebird");
+//const Promise=require("bluebird");
 const fs=require("fs");
 //const fileread=Promise.promisify(fs.readFile);
 //const filename=__dirname+"/Json_files/file1.json";
@@ -14,7 +14,8 @@ app.get('/',(req,res)=>{
 })
 app.get('/:username',(req,res)=>{
     res.header("Access-Control-Allow-Origin", "*");
-    //dbresult=db2con.connect(req.params.username);
-    res.send(db2con.connect(req.params.username));
+    let dbresult=db2con.connect(req.params.username);
+    console.log(dbresult);
+    res.send(dbresult);
 })
 app.listen(3000);
