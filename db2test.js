5448fb405daff230fda35487e54c29f8db9d4bd9
ZITAVISH <rounakdas2014@gmail.com>
2019-12-15 16:05:49 +0530

    Old version

--- db2test.js
+++ db2test.js
@@ -1,37 +1,22 @@
-var ibmdb = require("ibm_db"),
-  //connString = "DATABASE=database;HOSTNAME=hostname;PORT=port;PROTOCOL=TCPIP;UID=username;PWD=password;";
-  connString = "DATABASE=TEST;HOSTNAME=localhost;UID=ROUNAK;PWD=Farcry@5;PORT=50000;PROTOCOL=TCPIP";
-module.exports.connect=function(matcher){
+var ibmdb = require('ibm_db');
 let result;
-try {
-  var option = { connectTimeout : 40, systemNaming : true };// Connection Timeout after 40 seconds.
-  var conn = ibmdb.openSync(connString, option);
+module.exports.connect=(matcher)=>{
+ibmdb.open("DATABASE=TEST;HOSTNAME=localhost;UID=ROUNAK;PWD=Farcry@5;PORT=50000;PROTOCOL=TCPIP", function (err,conn) {
+  if (err) return console.log(err);
   let value=matcher;
   let queryst=`select * from CROUTINE as CROUTINE where TNAME='${value}'`;
- let res=conn.querySync(queryst);
-  return res;
-} catch (e) {
-  console.log(e.message);
-}
-//console.log("Outside function");
-//console.log("this is returned value");
-//return result;
-};
-/**
- * , function (err, rows) {
-    if (err) {
-      console.log(err);
-    }
+  conn.query(queryst, function (err, data) {
+    if (err) console.log(err);
     else {
-      result = rows;
+    console.log(data);
+    result=data;
     }
-    console.log("Inside function");
-    console.log(result);
-    
-    conn.closeSync();
-    console.log("Inside function after con close");
-    console.log(result);
-    return result;
-    //console.log(rows);
- * 
- */
+    conn.close(function () {
+      console.log('connection closed');
+    });
+  });
+});
+console.log(result);
+return result;
+}
+module.exports.resul=result;
